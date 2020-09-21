da03b3a673c9ff4a10c3fc519dd4bc8c07cdff8a
Coronia <2217891145@qq.com>
2020-09-21 01:07:19 -0400

    city

--- 01-python/leisure/restaurant.py
+++ 01-python/leisure/restaurant.py
@@ -1,3 +1,7 @@
 def draw_restaurant():
-    print("restaurant not found")
+    print(" ||| ")
+    print(" \|/ ")
+    print("  |  ")
+    print("  |  ")
+    print("  |  ")
     return
--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -1,3 +1,5 @@
 def draw_hospital():
-    print("hospital not found")
+    print("   |   ")
+    print(" --+-- ")
+    print("   |   ")
     return
