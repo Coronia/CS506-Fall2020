b634bd74339fac08b78577da1712a8d61ce5f994
Coronia <2217891145@qq.com>
2020-09-21 21:57:04 -0400

    library

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,17 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+    with open(csv_file_path, 'r') as file:
+        lines = csv.reader(file)
+        res = []
+        for line in lines:
+            for i in range(len(line)):
+                if line[i].isdigit():
+                    line[i] = int(line[i])
+            res.append(line)
+    return res
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,43 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        return "Invalid input."
+    res = 0
+    for i in range(len(x)):
+        res += (x[i] - y[i]) ** 2
+    res **= (1 / 2)
+    return res
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        return "Invalid input."
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if x == y:
+        return 0
+    X = set(x)
+    Y = set(y)
+    res = 1 - len(X.intersection(Y)) / len(X.union(Y))
+    return res
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        return "Invalid input."
+    res = 0
+    a = 0
+    b = 0
+    for i in range(len(x)):
+        res += x[i] * y[i]
+        a += x[i] ** 2
+        b += y[i] ** 2
+    a **= (1 / 2)
+    b **= (1 / 2)
+    if a == 0 or b == 0:
+        return 0
+    res = res / (a * b)
+    return res
 
 # Feel free to add more
